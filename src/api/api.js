b868beb288871d024bb001f9b9a5a27e48f8302d
Abdul12Ahad <abdulahad6336123@gmail.com>
2026-01-27 20:20:02 +0530

    new changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://digittence.netlify.app/api",
+  baseURL: "https://digittencebe-production.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
